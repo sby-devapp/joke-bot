f3d97085198dcf466db1c549207d97040c955ae4
sbaitipro <abdelkhalek.sbaiti.pro@gmail.com>
2025-05-21 21:15:36 +0100

    add requirement

--- bot_token.py
+++ bot_token.py
@@ -1,10 +1,9 @@
-# export BOT_TOKEN="7460388943:AAFpQi4XupuZBgc59eC2ibV4wh0BE1dBiGI"
-
 import os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 if not BOT_TOKEN:
     raise ValueError("No BOT_TOKEN set in environment variables!")
 
+
 if __name__ == "__main__":
-    print(f"BOT_TOKEN: {BOT_TOKEN} / is set correctly.")
+    print(BOT_TOKEN)
